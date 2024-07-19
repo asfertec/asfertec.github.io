18b1f2b80bbb3565e16829620407a8d672970295
asfertec <72183614+asfertec@users.noreply.github.com>
2024-07-19 00:12:34 -0400

    Update scripts.js
    edicion 1

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
 document.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault();
-    alert('Formulario enviado');
+    alert('Formulario enviado. Nos pondremos en contacto contigo pronto.');
 });
